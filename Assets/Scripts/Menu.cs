e30fdff5cde7e3875bd5e563f08216950c6755e9
Kevin Grandemange <grandemange.kevin@gmail.com>
2019-10-05 14:28:10 +0200

    Escape key makes you go back to the menu.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private int main_menu_scene_index = 0;
+    void Update()
+    {
+        SceneLoader loader = GetComponent<SceneLoader>();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            loader.LoadByIndex(main_menu_scene_index);
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadGame : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     public void LoadByIndex(int scene_index)
     {
