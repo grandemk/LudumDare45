01286aa050b3eb9374943ca46f245e6138735436
Kevin Grandemange <grandemange.kevin@gmail.com>
2019-10-05 18:13:38 +0200

    Update Game scene.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float speed = 0.01f;
+    void Update()
+    {
+        transform.Translate(speed * new Vector3(0, 1), Space.World);
+    }
+}
