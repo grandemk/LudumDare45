b07954142c38f1eaf402e1fd696d8ea3d05a6fe0
Kevin Grandemange <grandemange.kevin@gmail.com>
2019-10-05 12:25:57 +0200

    Add very basic player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontal_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal_input, vertical_input, 0);
+
+        transform.Translate(Time.deltaTime * speed * direction);
+    }
+}
